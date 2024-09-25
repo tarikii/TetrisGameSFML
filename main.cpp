d0bc1d1869d3eeaa30c470c4060c35e26cf12077
tarikii <tarikaabouchazougarh@gmail.com>
2024-09-25 12:50:45 +0200

    Window created

--- main.cpp
+++ main.cpp
@@ -5,26 +5,17 @@ int main()
 {
     // create the window
     sf::RenderWindow window(sf::VideoMode(400, 400), "TetrisGameSFML");
+    window.setFramerateLimit(60);
 
     // run the program as long as the window is open
     while (window.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // "close requested" event: we close the window
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
-        // clear the window with black color
-        window.clear(sf::Color::Black);
-
-        // draw everything here...
-        // window.draw(...);
-
-        // end the current frame
+        window.clear(sf::Color(20, 20, 20));
         window.display();
     }
 
